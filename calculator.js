855e820bb9bdfbb9446fddc64c178c599ccd90ae
martabalbo <marta99.balbo@gmail.com>
2026-06-05 12:21:26 +0200

    Add operate function

--- calculator.js
+++ calculator.js
@@ -1,9 +1,29 @@
 const add = (a, b) => a + b;
-
 const subtract = (a, b) => a - b;
-
 const multiply = (a, b) => a * b;
-
 const divide = (a, b) => a / b;
 
-console.log(add(2,3), subtract(2,3), multiply(2,3), divide(2,3));
+let firstOperand = 0;
+let secondOperand = 0;
+let operator = "";
+
+function operate(a, b, c) {
+    let result = "ERR";
+    switch (c) {
+        case "+":
+            result = add(a, b);
+            break;
+        case "-":
+            result = subtract(a, b);
+            break;
+        case "*":
+            result = multiply(a, b);
+            break;
+        case "/":
+            result = divide(a, b);
+            break;
+    };
+    return result;
+};
+
+console.log(operate(2,3,"-"))
