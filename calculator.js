f7a9ae6e0d994ab5c267750ca92973f98e1ae3da
martabalbo <marta99.balbo@gmail.com>
2026-06-05 12:09:08 +0200

    Crate javascript file and add sum, subtract, multiply and divide functions

---
+++ calculator.js
@@ -0,0 +1,9 @@
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => a / b;
+
+console.log(add(2,3), subtract(2,3), multiply(2,3), divide(2,3));
