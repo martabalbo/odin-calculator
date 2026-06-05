9d772f53f9c9daeeb845ef8e0ad3adfb51e3655f
martabalbo <marta99.balbo@gmail.com>
2026-06-05 17:16:27 +0200

    Addfunctions to perform basic caluclations

--- calculator.js
+++ calculator.js
@@ -1,4 +1,4 @@
-const add = (a, b) => a + b;
+const add = (a, b) => Number(a) + Number(b);
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
@@ -23,28 +23,53 @@ function operate(a, b, c) {
             result = divide(a, b);
             break;
     };
-    return result;
+    return Number(result.toFixed(5));
 };
 
 const numberButtons = Array.from(document.querySelectorAll(".number"));
-const operatorButtons = Array.from(document.querySelectorAll(".operator"));
 const display = document.querySelector(".display");
 
 numberButtons.forEach((button) => {
     button.addEventListener("click" , () => {
-
-        /*if (firstOperand === "") {
-            firstOperand = button.id;
+        if (operator == "") {
+            firstOperand = firstOperand + button.id;
             display.textContent = firstOperand;
         } else {
-            secondOperand = button.id;
-            display.textContent = firstOperand + " " + secondOperand;
-        };*/
+            secondOperand = secondOperand + button.id;
+            display.textContent = firstOperand + operator + secondOperand;
+        };
     });
 });
 
+const operatorButtons = Array.from(document.querySelectorAll(".operator"));
 
-const buttons = Array.from(document.querySelectorAll("button"));
+operatorButtons.forEach((button) => {
+    button.addEventListener("click" , () => {
+        if (operator != "") {
+            firstOperand = operate(firstOperand, secondOperand, operator);
+            secondOperand = "";
+        }
+        operator = button.id;
+        display.textContent = firstOperand + operator;
+    });
+});
+
+const equalButton = document.querySelector(".equals");
+
+equalButton.addEventListener("click", () => {
+    firstOperand = operate(firstOperand, secondOperand, operator);
+    secondOperand = "";
+    operator = "";
+    display.textContent = firstOperand;
+});
+
+//to do: if a result is displayed, make it not possible to add numbers to it.
+//instead substitute the number.
+
+
+
+//panic button
+/*const buttons = Array.from(document.querySelectorAll("button"));
 buttons.forEach((button) => {
     button.addEventListener("click", () => {display.textContent = "sadanbdhusnah anhAAAAAAAAAAAAAAAHHHHHH"});
-});
+});*/
